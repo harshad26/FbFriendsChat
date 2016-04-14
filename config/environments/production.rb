f69a253d2f4b90d6d764fbbe3688b20824398fa1
Harshad <patelharshad1@gmail.com>
2016-04-14 19:21:45 +0530

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'tranquil-brushlands-87976.herokuapp.com' }
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-                                        address:              'smtp.gmail.com',
-                                        port:                  25,
-                                        domain:               'gmail.com',
-                                        user_name:            'itserviceyouth@gmail.com',
-                                        password:             'Badashah26',
-                                        authentication:       'plain',
-                                        enable_starttls_auto: true  
-                                      }
-
+      :enable_starttls_auto => true,
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :authentication => :login,
+      :user_name => "itserviceyouth@gmail.com",
+      :password => "Badashah26"
+  }
 
 end
