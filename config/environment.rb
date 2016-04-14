bb8a089bee3fb2128b7a00656c0613499cae587c
Harshad <patelharshad1@gmail.com>
2016-04-14 19:44:28 +0530

    changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+:address => 'smtp.gmail.com',
+:port => 587,
+:user_name => "itserviceyouth@gmail.com",
+:password => "Badashah26",
+:authentication => :plain,
+:enable_starttls_auto => true,
+:openssl_verify_mode => 'none' 
+}
