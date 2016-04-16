9d938b82473cc2518c986f82caa2419110e8d7a6
Harshad <patelharshad1@gmail.com>
2016-04-16 16:42:28 +0530

    responsive

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -7,27 +7,31 @@ module HomeHelper
 		@me_latitude = current_user.latitude
 		@me_longitude = current_user.longitude
 		
-		@friendDetail = User.find_by_uid(fbid)
-		
-		if !@friendDetail.latitude.nil?
-			@frd_latitude = @friendDetail.latitude
-			@frd_longitude = @friendDetail.longitude
+		if !@me_latitude.nil?
+			@friendDetail = User.find_by_uid(fbid)
 			
-			radius = 6371
-		    lat1 = self.to_rad(@me_latitude)
-		    lat2 = to_rad(@frd_latitude)
-		    lon1 = to_rad(@me_longitude)
-		    lon2 = to_rad(@frd_longitude)
-		    dLat = lat2-lat1  
-		    dLon = lon2-lon1
-		    a = Math::sin(dLat/2) * Math::sin(dLat/2) +
-		       Math::cos(lat1) * Math::cos(lat2) * 
-		       Math::sin(dLon/2) * Math::sin(dLon/2);
-		    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a));
-		    @d = (radius * c).round
+			if !@friendDetail.latitude.nil?
+				@frd_latitude = @friendDetail.latitude
+				@frd_longitude = @friendDetail.longitude
+				
+				radius = 6371
+			    lat1 = self.to_rad(@me_latitude)
+			    lat2 = to_rad(@frd_latitude)
+			    lon1 = to_rad(@me_longitude)
+			    lon2 = to_rad(@frd_longitude)
+			    dLat = lat2-lat1  
+			    dLon = lon2-lon1
+			    a = Math::sin(dLat/2) * Math::sin(dLat/2) +
+			       Math::cos(lat1) * Math::cos(lat2) * 
+			       Math::sin(dLon/2) * Math::sin(dLon/2);
+			    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a));
+			    @d = (radius * c).round
+			else
+				@d = 'No found'
+			end
 		else
-			@d = ''
-		end    
+			@d = 'No found'
+		end	    
 	    return @d
     end
 
