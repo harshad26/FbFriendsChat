c8e72aab7a4172cf3a9a5a7a5f52f30c235bb51f
Harshad <patelharshad1@gmail.com>
2016-04-26 17:48:40 +0530

    app controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,9 +26,9 @@ class ApplicationController < ActionController::Base
   end
 
   def check_messages
-    if session[:mark_messages] and session[:mark_messages] != 1 and current_user
+    if (session[:mark_messages].nil? or session[:mark_messages] != 1) and current_user
       @useConversations = Message.select("conversation_id").where("user_id = (?)", current_user.id).count
-      if @useConversations.count > 0
+      if @useConversations > 0
         @useConversations = Message.where("user_id = (?)", current_user.id).pluck(:conversation_id)
         @useConversations = @useConversations.uniq # Unique
         @useConversations = @useConversations.map(&:inspect).join(', ')
