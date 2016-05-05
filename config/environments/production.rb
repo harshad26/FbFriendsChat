38bf4d6167849af7ef74fc633a7ef05915a4fe8a
Harshad <patelharshad1@gmail.com>
2016-05-05 12:20:08 +0530

    add production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
       :port => 587,
       :domain => "gmail.com",
       :authentication => :plain,
-      :user_name => "rpatil1390@gmail.com",
-      :password => "sairam11"
+      :user_name => "itserviceyouth@gmail.com",
+      :password => "Badashah26"
   }
 
 end
