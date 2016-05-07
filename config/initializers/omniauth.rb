8cbaccaadefce37b6badf553cf46a436e177acb5
Harshad <patelharshad1@gmail.com>
2016-05-07 16:06:39 +0530

    add omniauth setting

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,"68150529157","4f60b22d0bffc2d32e08be47c107e2e1", :scope => "user_friends"
-  # provider :facebook,ENV["FACEBOOK_APP_ID"],ENV["FACEBOOK_SECRET"], :scope => "user_friends"
+  provider :facebook,ENV["FACEBOOK_APP_ID"],ENV["FACEBOOK_SECRET"], :scope => "user_friends"
 end
